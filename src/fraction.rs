a57c96d49114eeb81fc9c3080fe65012b2d5249e
bgkillas <bgkillas.mw@gmail.com>
2023-05-14 09:12:44 -0400

    use chars instead of strings order in order of commonality dont iter i for the for loop

--- src/fraction.rs
+++ src/fraction.rs
@@ -5,6 +5,7 @@ pub fn fraction(num:f64) -> String
     {
         return "0".to_string();
     }
+    // as per continued fraction expansion
     // 0.6
     // 1.666 -> 0.666
     // 1.5 -> 0.5
@@ -18,18 +19,20 @@ pub fn fraction(num:f64) -> String
     // 0.6 = 3/5
     // therefore 3/5 is the fraction
     let mut nums:Vec<f64> = vec![];
-    let values = [(1.0, "1"), (3f64.sqrt(), "3"), (2f64.sqrt(), "2"), (PI, "p")];
-    let mut which = "";
-    let mut number = num;
+    let values = [(1.0, '1'), (PI, 'p'), (2f64.sqrt(), '2'), (3f64.sqrt(), '3')];
+    let mut which = '1';
+    let mut number = 0.0;
     let mut c = 0;
+    let mut recip;
+    let mut fract;
     'outer: for &(constant, name) in &values
     {
         number = num / constant;
         nums.clear();
-        for i in 0..=10
+        for _ in 0..=10
         {
-            let recip = number.recip();
-            let fract = recip.fract();
+            recip = number.recip();
+            fract = recip.fract();
             if (fract * 1e9).round() / 1e9 == 0.0
             {
                 number = (recip * 1e9).round() / 1e9;
@@ -38,11 +41,8 @@ pub fn fraction(num:f64) -> String
             }
             nums.push(recip);
             number = fract;
-            if i == 10
-            {
-                c += 1;
-            }
         }
+        c += 1;
     }
     if c == values.len()
     {
@@ -55,13 +55,13 @@ pub fn fraction(num:f64) -> String
         number *= nums[nums.len() - 1 - i];
     }
     format!("{}{}{}",
-            if last.round() == 1.0 && which != "1" { "".to_string() } else { last.round().to_string() },
+            if last.round() == 1.0 && which != '1' { "".to_string() } else { last.round().to_string() },
             match which
             {
-                "1" => "",
-                "3" => "sqrt(3)",
-                "2" => "sqrt(2)",
-                "p" => "π",
+                '1' => "",
+                '3' => "sqrt(3)",
+                '2' => "sqrt(2)",
+                'p' => "π",
                 _ => "",
             },
             if number.round() == 1.0 { "".to_string() } else { "/".to_owned() + &number.round().to_string() })

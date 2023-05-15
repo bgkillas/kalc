298674df27a18eb3bd0732d315894e697154cd37
bgkillas <bgkillas.mw@gmail.com>
2023-05-15 18:17:56 -0400

    fix vars not working

--- src/main.rs
+++ src/main.rs
@@ -427,6 +427,10 @@ fn equal(range:([[f64; 2]; 3], f64, f64), input:&str, l:&str, r:&str) -> bool
         println!("false");
         return true;
     }
+    if l.parse::<f64>().is_err()
+    {
+        return false;
+    }
     let l = match do_math(match get_func(l, false)
           {
               Ok(i) => i,

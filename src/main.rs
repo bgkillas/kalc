5d4663dff4974d52d0b03e472669ddb0a89a487b
eddi <108631435+LowLevelLoser@users.noreply.github.com>
2023-08-23 15:30:31 -0500

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -295,7 +295,7 @@ fn main()
                 }
                 match c
                 {
-                    '\n' =>
+                    '\n' => 
                     {
                         end = start + get_terminal_width() - if options.prompt { 3 } else { 0 };
                         if end > input.len()
@@ -322,7 +322,7 @@ fn main()
                         end = 0;
                         break;
                     }
-                    '\x08' =>
+                    '\x08' => //Backspace
                     {
                         if placement - start == 0 && start != 0
                         {
@@ -338,6 +338,68 @@ fn main()
                             continue;
                         }
                         placement -= 1;
+                        input.remove(placement);
+                        end = start + get_terminal_width() - if options.prompt { 3 } else { 0 };
+                        if end > input.len()
+                        {
+                            end = input.len()
+                        }
+                        if i == max
+                        {
+                            current = input.clone();
+                        }
+                        else
+                        {
+                            lines[i as usize] = input.clone().iter().collect::<String>();
+                        }
+                        frac = if input.is_empty()
+                        {
+                            0
+                        }
+                        else if options.real_time_output && !input.is_empty()
+                        {
+                            print_concurrent(&input, &last, &vars, options, start, end)
+                        }
+                        else
+                        {
+                            clear(&input, start, end, options);
+                            0
+                        };
+                        if let Some(time) = watch
+                        {
+                            let time = time.elapsed().as_nanos();
+                            print!(
+                                " {}{}",
+                                time,
+                                "\x08".repeat(
+                                    time.to_string().len() + 1 + end - start - (placement - start)
+                                )
+                            );
+                        }
+                        else
+                        {
+                            print!("{}", "\x08".repeat(end - start - (placement - start)));
+                        }
+                        if placement == 0 && input.is_empty()
+                        {
+                            clear(&input, start, end, options);
+                        }
+                    }
+                    '\x7F' => //Delete
+                    {
+                        if placement - start == 0 && start != 0
+                        {
+                            start -= 1;
+                        }
+                        if placement == end
+                        {
+                            if input.is_empty()
+                            {
+                                clear(&input, start, end, options);
+                                stdout().flush().unwrap();
+                            }
+                            continue;
+                        }
                         input.remove(placement);
                         end = start + get_terminal_width() - if options.prompt { 3 } else { 0 };
                         if end > input.len()

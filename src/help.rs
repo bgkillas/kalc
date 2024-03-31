0d16f93363e6211fb42b544690bd6c08e8a66168
bgkillas <walhelm@proton.me>
2024-03-31 09:36:54 -0400

    get rid of deg/rad/grad functions as they conflict when doing powers to degree units like 1deg^2 , get rid of sr as base unit and replace it with rad^2, work with plural units,

--- src/help.rs
+++ src/help.rs
@@ -99,7 +99,6 @@ Functions:\x1b[G\n\
 - fact, doublefact, subfact\x1b[G\n\
 - sinc, cis, exp\x1b[G\n\
 - zeta, gamma, beta, erf, erfc, digamma, ai, multinomial, binomial/bi/C(n,r), P(n,r), pochhammer(x,n)\x1b[G\n\
-- deg, rad, grad\x1b[G\n\
 - re, im, split(x+yi), next(n,to)\x1b[G\n\
 - factors, prime, isprime, gcd, lcm\x1b[G\n\
 - slog(a,b), ssrt(k,a) (k is lambert w branch)\x1b[G\n\

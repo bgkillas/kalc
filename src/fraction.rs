8be1b724fac7b51877eba803602820717549a2ab
bgkillas <walhelm@proton.me>
2024-05-12 09:51:55 -0400

    detect general sqrt fractions, ie sqrt(8)/sqrt(3) detects as sqrt(8/3), detect 1/e and e for fractions,fix a rounding error making 1.09999999999999999 output as 2,

--- src/fraction.rs
+++ src/fraction.rs
@@ -1,20 +1,20 @@
 // as per continued fraction expansion
 use crate::Options;
-use rug::{float::Constant::Pi, Float};
+use rug::{float::Constant::Pi, ops::Pow, Float, Integer};
 pub fn fraction(value: Float, options: Options) -> String
 {
     if value.clone().fract().is_zero() || !value.is_finite()
     {
         return String::new();
     }
-    let rt3 = Float::with_val(options.prec, 3).sqrt();
+    let e = Float::with_val(options.prec, 1.0).exp();
     let values = [
-        Float::with_val(options.prec, 1.0),
-        Float::with_val(options.prec, Pi),
-        Float::with_val(options.prec, 2).sqrt(),
-        rt3.clone(),
-        Float::with_val(options.prec, 2 + rt3.clone()).sqrt(),
-        Float::with_val(options.prec, 2 - rt3).sqrt(),
+        None,
+        None,
+        None,
+        Some(Float::with_val(options.prec, Pi)),
+        Some(e.clone()),
+        Some(1 / e),
     ];
     let sign: String = if value < 0.0
     {
@@ -25,15 +25,45 @@ pub fn fraction(value: Float, options: Options) -> String
         String::new()
     };
     let val = value.abs();
-    let tau = "π";
     for (i, constant) in values.iter().enumerate()
     {
-        let orig = val.clone() / constant;
+        let orig = if i == 0
+        {
+            val.clone()
+        }
+        else if i == 1
+        {
+            val.clone().pow(2)
+        }
+        else if i == 2
+        {
+            val.clone().pow(3)
+        }
+        else
+        {
+            val.clone() / constant.clone().unwrap()
+        };
         if orig.clone().fract().is_zero()
         {
             return if i == 0
             {
                 String::new()
+            }
+            else if i == 1 || i == 2
+            {
+                format!(
+                    "{}{}({})",
+                    sign,
+                    if i == 1 { "sqrt" } else { "cbrt" },
+                    if orig == 1.0
+                    {
+                        String::new()
+                    }
+                    else
+                    {
+                        orig.to_integer().unwrap().to_string()
+                    }
+                )
             }
             else
             {
@@ -50,11 +80,9 @@ pub fn fraction(value: Float, options: Options) -> String
                     },
                     match i
                     {
-                        1 => tau,
-                        2 => "sqrt(2)",
-                        3 => "sqrt(3)",
-                        4 => "sqrt(2+sqrt(3))",
-                        5 => "sqrt(2-sqrt(3))",
+                        3 => "π",
+                        4 => "e",
+                        5 => "1/e",
                         _ => "",
                     }
                 )
@@ -62,11 +90,11 @@ pub fn fraction(value: Float, options: Options) -> String
         }
         let mut number = orig.clone().fract();
         let mut nums = Vec::new();
-        for _ in 0..=50
+        for _ in 0..32
         {
             let mut recip = number.clone().recip();
             let fract = recip.clone().fract();
-            if fract < 1e-36
+            if fract < 1e-32
             {
                 let mut last = Float::with_val(options.prec, 1);
                 for j in (0..nums.len()).rev()
@@ -86,6 +114,49 @@ pub fn fraction(value: Float, options: Options) -> String
                 {
                     String::new()
                 }
+                else if i == 1 || i == 2
+                {
+                    format!(
+                        "{sign}{}({}{}",
+                        if i == 1 { "sqrt" } else { "cbrt" },
+                        if last == 1 && i != 0
+                        {
+                            String::new()
+                        }
+                        else
+                        {
+                            last.to_string()
+                        },
+                        if recip == 1
+                        {
+                            String::new()
+                        }
+                        else if i == 1
+                        {
+                            let (root, rem) = recip.clone().sqrt_rem(Integer::new());
+                            if rem == 0
+                            {
+                                ")/".to_owned() + &root.to_string()
+                            }
+                            else
+                            {
+                                "/".to_owned() + &recip.to_string() + ")"
+                            }
+                        }
+                        else
+                        {
+                            let (root, rem) = recip.clone().root_rem(Integer::new(), 3);
+                            if rem == 0
+                            {
+                                ")/".to_owned() + &root.to_string()
+                            }
+                            else
+                            {
+                                "/".to_owned() + &recip.to_string() + ")"
+                            }
+                        }
+                    )
+                }
                 else
                 {
                     format!(
@@ -100,15 +171,24 @@ pub fn fraction(value: Float, options: Options) -> String
                         },
                         match i
                         {
-                            0 => "",
-                            1 => tau,
-                            2 => "sqrt(2)",
-                            3 => "sqrt(3)",
-                            4 => "sqrt(2+sqrt(3))",
-                            5 => "sqrt(2-sqrt(3))",
-                            _ => "",
+                            0 => String::new(),
+                            3 => "π".to_string(),
+                            4 => "e".to_string(),
+                            5 => last.to_string(),
+                            _ => String::new(),
                         },
-                        if recip == 1
+                        if i == 5
+                        {
+                            if recip == 1
+                            {
+                                "/".to_owned() + "e"
+                            }
+                            else
+                            {
+                                "/".to_owned() + "(" + &recip.to_string() + "e" + ")"
+                            }
+                        }
+                        else if recip == 1
                         {
                             String::new()
                         }
